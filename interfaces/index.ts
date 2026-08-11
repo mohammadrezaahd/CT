89c78065bb1d83515f2a259a5680f0d85a8a6b50
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2026-08-11 17:49:21 +0330

    Fix build issue

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,6 +2,6 @@ export * from "./Friendship.interface";
 export * from "./General.interface";
 export * from "./Program.interface";
 export * from "./Progress.interface";
-export * from "./User.interface";
+// export * from "./User.interface";
 export * from "./Workout.interface";
 export * from "./Interfces";
